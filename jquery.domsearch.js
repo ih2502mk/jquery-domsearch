72ff5ad92e6137a54063d6a2a93f89465479fb1f
ih2502mk <ih2502mk@gmail.com>
2011-02-23 19:49:14 +0200

    Changed the way that input field reactds on userinput. Added "Delayed" constructor. It sets timeout on first keyup and cancels timeout if user continues input. This causes searching function to be invoked less often.

--- jquery.domsearch.js
+++ jquery.domsearch.js
@@ -20,7 +20,7 @@ $(function($) {
         case Array:
           text = $.map(
             options.criteria,
-            function(crit) { return $(row).find(crit).text(); }
+            function(crit) {return $(row).find(crit).text();}
           ).join(' ');
           break;
         case String:
@@ -33,42 +33,65 @@ $(function($) {
       $(row).show().data('domsearch.score', LiquidMetal.score(text, query));
       return $(row).data('domsearch.score') < options.minimumScore;
     })
-    .sort(function(a, b) { return $(a).data('domsearch.score') < $(b).data('domsearch.score'); }))
+    .sort(function(a, b) {return $(a).data('domsearch.score') < $(b).data('domsearch.score');}))
       .appendTo(searchIn)
       .hide();
   }
 
   function init(element, searchIn, options) {
     var target = $(searchIn),
-      defaults = { unit: undefined, criteria: false, minimumScore: 0.5 },
+      defaults = {unit: undefined, criteria: false, minimumScore: 0.5},
       opts = $.extend(defaults, options);
     opts.unit = opts.unit || guessUnit(target[0].tagName);
 
     var originalOrder = target.find(opts.unit);
 
-    $(element).keydown(function(event) {
+		//Delayer manages timeout clearing so searching function is only invoked once
+		//after user has entered the query
+		var Delayer = function(timeout){
+			this.timeout = timeout ? timeout : 100;
+		}
+
+		Delayer.prototype.setup = function(delayedFn){
+			this.clear();
+			this.timerId = setTimeout(delayedFn, this.timeout);
+		}
+
+		Delayer.prototype.clear = function(){
+			if(typeof this.timerId == "number"){
+				clearTimeout(this.timerId);
+				delete this.timerId;
+			}
+		}
+
+		var delayer = new Delayer(400);
+
+    $(element).keyup(function(event) {
       if (event.keyCode == 9) return true; // TAB
       var field = $(this);
-      setTimeout(
-        function() {
-          if (field.val() == '') {
-            originalOrder.show().appendTo(target);
-          } else {
-            search(field.val(), target[0], opts);
-          }
-          if (typeof opts.onkeydown == 'function') opts.onkeydown(field);
-        },
-      100);
+				delayer.setup(
+
+					function() {
+						console.log('fn');
+						if (field.val() == '') {
+							originalOrder.show().appendTo(target);
+						} else {
+							search(field.val(), target[0], opts);
+						}
+						if (typeof opts.onkeydown == 'function') opts.onkeydown(field);
+					}
+
+				);
       return true;
     });
   }
 
-  $.fn.sort       = function() { return this.pushStack([].sort.apply(this, arguments), []); };
-  $.domsearch     = function(element, searchIn, options) { init(element, searchIn, options); };
+  $.fn.sort       = function() {return this.pushStack([].sort.apply(this, arguments), []);};
+  $.domsearch     = function(element, searchIn, options) {init(element, searchIn, options);};
   $.fn.domsearch  = function(query, options) {
     if (!$(this).data('domsearch.enabled')) {
       $(this).data('domsearch.enabled', true);
-      return this.each(function() { new $.domsearch(this, query, options); });
+      return this.each(function() {new $.domsearch(this, query, options);});
     }
   };
 });
