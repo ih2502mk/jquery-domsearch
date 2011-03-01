67f74e559031f8c1348bfa4d1779ad4ce54d6113
ih2502mk <ih2502mk@gmail.com>
2011-03-01 13:58:56 +0200

    deleted css bak

--- jquery.domsearch.js
+++ jquery.domsearch.js
@@ -1,97 +1,144 @@
 $(function($) {
-  function guessUnit(tagName) {
-    switch(tagName) {
-      case 'TBODY':
-      case 'TABLE':
-        return 'tr';
-      case 'OL':
-      case 'UL':
-        return 'li';
-      case 'DIV':
-        return 'div';
-    }
-    return undefined;
-  }
+	var Delayer = function(timeout){
+		this.timeout = timeout ? timeout : 100;
+	}
 
-  function search(query, searchIn, options) {
-    $($.grep($(searchIn).find(options.unit), function(row) {
-      var text;
-      switch(options.criteria.constructor) {
-        case Array:
-          text = $.map(
-            options.criteria,
-            function(crit) {return $(row).find(crit).text();}
-          ).join(' ');
-          break;
-        case String:
-          text = $(row).find(options.criteria).text();
-          break;
-        default:
-          text = $(row).text();
-          break;
-      }
-      $(row).show().data('domsearch.score', LiquidMetal.score(text, query));
-      return $(row).data('domsearch.score') < options.minimumScore;
-    })
-    .sort(function(a, b) {return $(a).data('domsearch.score') < $(b).data('domsearch.score');}))
-      .appendTo(searchIn)
-      .hide();
-  }
+	Delayer.prototype.setup = function(delayedFn){
+		this.clear();
+		this.timerId = setTimeout(delayedFn, this.timeout);
+	}
 
-  function init(element, searchIn, options) {
-    var target = $(searchIn),
-      defaults = {unit: undefined, criteria: false, minimumScore: 0.5},
-      opts = $.extend(defaults, options);
-    opts.unit = opts.unit || guessUnit(target[0].tagName);
-
-    var originalOrder = target.find(opts.unit);
-
-		//Delayer manages timeout clearing so searching function is only invoked once
-		//after user has entered the query
-		var Delayer = function(timeout){
-			this.timeout = timeout ? timeout : 100;
+	Delayer.prototype.clear = function(){
+		if(typeof this.timerId == "number"){
+			clearTimeout(this.timerId);
+			delete this.timerId;
 		}
+	}
 
-		Delayer.prototype.setup = function(delayedFn){
-			this.clear();
-			this.timerId = setTimeout(delayedFn, this.timeout);
+	function guessUnit(tagName) {
+		switch(tagName) {
+			case 'TBODY':
+			case 'TABLE':
+				return 'tr';
+			case 'OL':
+			case 'UL':
+				return 'li';
+			case 'DIV':
+				return 'div';
+			case 'select':
+				return 'option';
 		}
+		return undefined;
+	}
 
-		Delayer.prototype.clear = function(){
-			if(typeof this.timerId == "number"){
-				clearTimeout(this.timerId);
-				delete this.timerId;
-			}
-		}
-
-		var delayer = new Delayer(400);
-
-    $(element).keyup(function(event) {
-      if (event.keyCode == 9) return true; // TAB
-      var field = $(this);
-				delayer.setup(
-
-					function() {
-						console.log('fn');
-						if (field.val() == '') {
-							originalOrder.show().appendTo(target);
-						} else {
-							search(field.val(), target[0], opts);
+	function search(query, searchIn, options){
+		$($.grep($(searchIn).find(options.unit), function(row) {
+			var text;
+			switch(options.criteria.constructor) {
+				case Array:
+					text = $.map(
+						options.criteria,
+						function(crit) {
+							return $(row).find(crit).text();
 						}
-						if (typeof opts.onkeydown == 'function') opts.onkeydown(field);
-					}
+						).join(' ');
+					break;
+				case String:
+					text = $(row).find(options.criteria).text();
+					break;
+				default:
+					text = $(row).text();
+					break;
+			}
+			$(row).show().data('domsearch.score', LiquidMetal.score(text, query));
+			return $(row).data('domsearch.score') < options.minimumScore;
+		})
+		.sort(function(a, b) {
+			return $(a).data('domsearch.score') < $(b).data('domsearch.score');
+		}))
+		.appendTo(searchIn)
+		.hide();
+	}
 
-				);
-      return true;
-    });
-  }
+	var options = {
+		unit: undefined,
+		criteria: false,
+		minimumScore: 0.5
+	}
 
-  $.fn.sort       = function() {return this.pushStack([].sort.apply(this, arguments), []);};
-  $.domsearch     = function(element, searchIn, options) {init(element, searchIn, options);};
-  $.fn.domsearch  = function(query, options) {
-    if (!$(this).data('domsearch.enabled')) {
-      $(this).data('domsearch.enabled', true);
-      return this.each(function() {new $.domsearch(this, query, options);});
+	var methods = {
+		init: function(searchIn, options) {
+			var settings = {
+				unit: undefined,
+				criteria: false,
+				minimumScore: 0.5
+			};
+
+			return this.each(function (){
+				var $this = $(this),
+						domSearchEnabled = $this.data('domsearch.enabled');
+
+
+				if(!domSearchEnabled || options ){
+					$.extend(settings, options);
+
+					settings.target = $(searchIn);
+					settings.unit = settings.unit || guessUnit(settings.target[0].tagName);
+
+					var delayer = new Delayer(400);
+
+					$this.keyup(function(event) {
+						if (event.keyCode == 9) return true; // TAB
+						delayer.setup(
+							function() {
+			          if ($this.val() == '') {
+			            $this.data('originalOrder').show().appendTo(settings.target);
+			          } else {
+									search($this.val(), settings.target[0], settings);
+			          }
+								if (typeof settings.onkeydown == 'function') settings.onkeydown($this);
+							}
+
+							);
+						return true;
+					});
+
+					$this.data('originalOrder', settings.target.find(settings.unit));
+					$this.data('settings', settings);
+					$this.data('domsearch.enabled', true);
+				}
+			});
+		},
+
+		search: function(query, searchIn, options){
+			search(query, searchIn, options);
+		},
+
+		originalOrder: function(){
+			var $this = $(this);
+			var settings = $this.data('settings');
+			$this.data('originalOrder', settings.target.find(settings.unit));
+		}
+	}
+
+	$.fn.domsearch = function( method ) {
+
+    // Method calling logic
+    if ( methods[method] ) {
+      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || typeof method === 'string' || !method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist' );
     }
+
   };
-});
+
+	$.domsearch = function(element, searchIn, options) {
+		$(element).domsearch(searchIn, options);
+	};
+
+	$.fn.sort = function() {
+		return this.pushStack([].sort.apply(this, arguments), []);
+	};
+})
